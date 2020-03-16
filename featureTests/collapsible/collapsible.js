4e9141a8422f81b0062e89b35b6a76c4d49ac7fe
Matt Williams <mattinzhuhai@outlook.com>
2020-03-16 09:52:45 -0700

    Indentation fix, js and html should have 2 spaces of html

--- featureTests/collapsible/collapsible.js
+++ featureTests/collapsible/collapsible.js
@@ -1,42 +1,42 @@
 
 //force reload the page (ie: prevent cache for this page) 
 function reloadThePage(){
-    window.location.reload();
+  window.location.reload();
 } 
 
 
 //initialize collapsible
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
 
 
-  document.getElementById("button1").addEventListener("click",addHazard);
+document.getElementById("button1").addEventListener("click",addHazard);
 
 
 
-  function addHazard () {
-  	var hazardList = document.getElementById('hazardList');
-  	var haz1 = 'fire';
-  	var haz2 = 'electricity';
+function addHazard () {
+  var hazardList = document.getElementById('hazardList');
+  var haz1 = 'fire';
+  var haz2 = 'electricity';
 
-  	var newHazard = '<li class="collection-item avatar">'+
-  		'<img src="images/yuna.jpg" alt="" class="circle">'+
-        '<span class="title">' +
-        haz1 +
-        '</span>'+
-        '<p>'+
-        haz2 + 
-        '<br> Second Line </p>'+
-        '<a href="#!" class="secondary-content">'+
-        '<i class="material-icons">grade</i></a></li>';
-    
-    hazardList.innerHTML += newHazard;
+  var newHazard = '<li class="collection-item avatar">'+
+    '<img src="images/yuna.jpg" alt="" class="circle">'+
+    '<span class="title">' +
+    haz1 +
+    '</span>'+
+    '<p>'+
+    haz2 + 
+    '<br> Second Line </p>'+
+    '<a href="#!" class="secondary-content">'+
+    '<i class="material-icons">grade</i></a></li>';
 
-
-//     hazardList.createElement('Li');
-//	 innerHTML(newHazard); 
+  hazardList.innerHTML += newHazard;
 
 
-  }
+  //     hazardList.createElement('Li');
+  //	 innerHTML(newHazard); 
+
+
+}
