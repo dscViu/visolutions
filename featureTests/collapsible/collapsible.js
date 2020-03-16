b76c9ca2fc42c8b83a67e0ba52edfa2a0c3f7cf6
Lenz Paul <34327253+bringbackthedog@users.noreply.github.com>
2020-03-15 23:41:29 -0700

    + dynamic vars in collapsible.js

--- featureTests/collapsible/collapsible.js
+++ featureTests/collapsible/collapsible.js
@@ -10,3 +10,33 @@ function reloadThePage(){
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
   });
+
+
+  document.getElementById("button1").addEventListener("click",addHazard);
+
+
+
+  function addHazard () {
+  	var hazardList = document.getElementById('hazardList');
+  	var haz1 = 'fire';
+  	var haz2 = 'electricity';
+
+  	var newHazard = '<li class="collection-item avatar">'+
+  		'<img src="images/yuna.jpg" alt="" class="circle">'+
+        '<span class="title">' +
+        haz1 +
+        '</span>'+
+        '<p>'+
+        haz2 + 
+        '<br> Second Line </p>'+
+        '<a href="#!" class="secondary-content">'+
+        '<i class="material-icons">grade</i></a></li>';
+    
+    hazardList.innerHTML += newHazard;
+
+
+//     hazardList.createElement('Li');
+//	 innerHTML(newHazard); 
+
+
+  }
